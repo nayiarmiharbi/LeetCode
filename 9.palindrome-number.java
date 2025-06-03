fb0548383a72b36cbc1a95536dae94336256e75d
nayiarmiharbi <miharbinayiar@gmail.com>
2025-06-03 19:52:10 +0600

    integer reversal implemented

--- 9.palindrome-number.java
+++ 9.palindrome-number.java
@@ -7,17 +7,13 @@
 // @lc code=start
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = String.valueOf(x);
-        int left = 0;
-        int right = s.length() - 1;
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
         }
-        return true;
+        return x == reversed || x == reversed / 10;
     }
 }
 // @lc code=end
